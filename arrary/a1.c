c67569a09b5f21962a3993e86acccd528e427040
Hari Shankar Upadhyay <hsupadhyay99@gmail.com>
2025-07-19 01:53:14 +0530

    arrary

---
+++ arrary/a1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+
+   char langs [10][15] = {
+      "PYTHON", "JAVASCRIPT", "PHP",
+      "NODE JS", "HTML", "KOTLIN", "C++",
+      "REACT JS", "RUST", "VBSCRIPT"
+   };
+
+   for (int i = 0; i < 10; i++){
+      printf("%s\n", langs[i]);
+   }
+   
+   return 0;
+}
